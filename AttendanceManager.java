4ea19db214791fdfa5abd6d23c11bd0ca0884283
Gianarches <Giandelegencia0419@gmail.com>
2025-12-13 12:53:41 +0800

    Update AttendanceManager.java

--- AttendanceManager.java
+++ AttendanceManager.java
@@ -1,22 +1,22 @@
-package javaapplication1.Java;
+    package javaapplication1.Java;
 
-class AttendanceManager {
-    
-     // Handles attendance update based on type:
-    // 1 = Present, 2 = Late, 3 = Absent
-    public static void record(Student s, int type) {
-        switch(type) {
-            case 1:
-                s.markPresent();
-                break;
-            case 2:
-                s.markLate();
-                break;
-            case 3:
-                s.markAbsent();
-                break;
-            default:    
-                System.out.println("Invalid attendance type");
+    class AttendanceManager {
+
+         // Handles attendance update based on type:
+        // 1 = Present, 2 = Late, 3 = Absent
+        public static void record(Student s, int type) {
+            switch(type) {
+                case 1:
+                    s.markPresent();
+                    break;
+                case 2:
+                    s.markLate();
+                    break;
+                case 3:
+                    s.markAbsent();
+                    break;
+                default:    
+                    System.out.println("Invalid attendance type");
+            }
         }
     }
-}
